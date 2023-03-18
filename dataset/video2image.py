5b3b92fa88b1fd5644240a42fade74fda77ea289
TopTen1004 <97684719+TopTen1004@users.noreply.github.com>
2023-03-18 11:22:34 -0500

    added

--- dataset/video2image.py
+++ dataset/video2image.py
@@ -1,22 +1,23 @@
 import cv2
 import os
-import subprocess
 
 video_path = 'video/00000031 (online-video-cutter.com).mp4'
-output_dir = 'images-video31/'
+output_dir = 'images/'
 
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
     
+j = 0
 def split_video_to_frames(video_path, output_dir):
     cap = cv2.VideoCapture(video_path)
     frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
     for i in range(frame_count):
-        success, frame = cap.read()
-        if success:
-            cv2.imwrite(os.path.join(output_dir, f'{i:06d}.jpg'), frame)
+        
+        if i % 6 == 0:
+            j = int(i/6) + 400
+            success, frame = cap.read()
+            if success:
+                cv2.imwrite(os.path.join(output_dir, f'{j:06d}.jpg'), frame)
     cap.release()
     
 split_video_to_frames(video_path, output_dir)
-
-subprocess.run(['labelImg', output_dir])
